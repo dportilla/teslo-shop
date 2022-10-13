1448a3cb1781f135acb5edde74f8c39df1da90ed
David Portilla <dportillalara@gmail.com>
2022-10-13 19:49:13 -0300

    env added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot()
+  ],
   controllers: [],
   providers: [],
 })
