8661925e1510de2a5f7d0485956ff7e5e474bb4a
David Portilla <dportillalara@gmail.com>
2022-10-13 20:03:47 -0300

    connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
   imports: [
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.PORT,
+      database: process.env.POSTGRES_DB,
+      username: process.env.POSTGRESS_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
   ],
   controllers: [],
   providers: [],
