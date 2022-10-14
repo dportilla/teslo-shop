3a7151e758b94d00d4c1be599861217b0d1fb509
David Portilla <dportillalara@gmail.com>
2022-10-13 23:26:59 -0300

    QueryBuilder

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,9 +1,11 @@
 import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
+import { validate as isUUID } from 'uuid'
 
 @Injectable()
 export class ProductsService {
@@ -27,13 +29,31 @@ export class ProductsService {
     }
   }
 
-  async findAll() {
-    return await this.productsRepository.find();
+  async findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+
+    return await this.productsRepository.find({
+      take: limit,
+      skip: offset,
+      //TODO: relaciones
+    });
   }
 
   async findOne(id: string) {
-    const product = await this.productsRepository.findOneBy({id});
+    let product: Product;
 
+    if (isUUID(id)) {
+      product = await this.productsRepository.findOneBy({id});
+
+    } else {
+      const queryBuilder = this.productsRepository.createQueryBuilder();
+      product = await queryBuilder
+        .where('title =:title or slug =:slug', {
+          title: id,
+          slug: id,
+        }).getOne();
+    }
+    
     if (!product) {
       throw new BadRequestException(`Product with id ${id} does not exist`);
     }
